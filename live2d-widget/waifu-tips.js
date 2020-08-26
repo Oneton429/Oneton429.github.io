c8d8161031c9092632fa9501d3e7d1610b7c4802
Oneton <oneton_429@outlook.com>
2020-08-26 11:13:29 +0800

    Site updated: 2020-08-26 11:13:29

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -162,7 +162,7 @@ function loadWidget(config) {
 			modelTexturesId = localStorage.getItem("modelTexturesId");
 		if (modelId === null) {
 			// 首次访问加载 指定模型 的 指定材质
-			modelId = 3; // 模型 ID
+			modelId = 2; // 模型 ID
 			modelTexturesId = 1; // 材质 ID
 		}
 		loadModel(modelId, modelTexturesId);
